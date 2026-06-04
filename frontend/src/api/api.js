d2694ba104ae57f8a11d57838eaf2846e6d10033
agrima29 <singhagrima089@gmail.com>
2026-06-04 22:20:30 +0530

    Connect frontend to Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "https://purplle-store-intelligence-dlca.onrender.com/api/v1",
 });
 
 export default API;
